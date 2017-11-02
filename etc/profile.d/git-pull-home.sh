f2b9ee52ffc2694fd642137dfd6b797df7ff98a6
jtingiris@dt <joseph.tingiris@gmail.com>
2017-11-02 11:07:23 -0400

    only pull if needed

--- etc/profile.d/git-pull-home.sh
+++ etc/profile.d/git-pull-home.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+UPSTREAM=${1:-'@{u}'}
+LOCAL=$(git rev-parse @)
+BASE=$(git merge-base @ "$UPSTREAM")
+
 # keep home directory up to date
 if [ -d ~/.git ]; then
     CWD=$(/usr/bin/pwd)
+
     cd ~
-    git pull &> /dev/null
+
+    if [ $LOCAL = $BASE ]; then
+        # need to pull
+        git pull &> /dev/null
+    fi
+
     cd "$CWD"
 fi
