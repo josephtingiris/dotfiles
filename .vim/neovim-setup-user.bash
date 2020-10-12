c5e1c8b943c2c95b8e40b2e9e25a1cfdf103d780
jtingiris@jt <jtingiris@jt>
2020-10-12 09:21:53 -0400

    username agnostic

--- .vim/neovim-setup-user.bash
+++ .vim/neovim-setup-user.bash
@@ -2,13 +2,13 @@
 
 # install/upgrade neovim user dependencies
 
-sudo chown -R $USER:"$(id -gn $USER)" /home/jtingiris/.config
+sudo chown -R $USER:"$(id -gn $USER)" ~/.config
 pip2 uninstall neovim
 pip2 install --user --upgrade pynvim
 pip3 uninstall neovim
 pip3 install --user --upgrade pynvim
 gem install --user-install neovim
-grep /home/jtingiris/.gem/ruby/bin ~/.Auto_Path || echo /home/jtingiris/.gem/ruby/bin >> ~/.Auto_Path
+grep .gem/ruby/bin ~/.Auto_Path || echo ~/.gem/ruby/bin >> ~/.Auto_Path
 npm install -g bash-language-server
 npm install -g typescript
 npm install -g neovim
